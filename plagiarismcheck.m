8deeaad80946ce930c66b9cf169438a9cc27ad30
mysablehats <mysablehats@gmail.com>
2017-06-12 01:23:35 +0100

    improved plagiarism checking

--- plagiarismcheck.m
+++ plagiarismcheck.m
@@ -6,16 +6,27 @@ function plagiarismcheck()
 AA = [];
 labels = [];
 for i = 1:100
-    if exist(['a' num2str(i) '.mat'], 'file')
-        aXX = load(['a' num2str(i) '.mat']);
-        AA = cat(2, AA, aXX.(['a' num2str(i)]));
+    aname = ['a' sprintf('%02d',i)];
+    if exist([aname '.mat'], 'file')
+        aXX = load([aname '.mat']);
+        AA = cat(2, AA, aXX.(aname));
         labels = [labels i];
+    else
+        for j = 1:4 %%% look for subparts
+            aname = ['a' sprintf('%02d',i) num2str(j)];
+            if exist([aname '.mat'], 'file')
+                aXX = load([aname '.mat']);
+                AA = cat(2, AA, aXX.(aname));
+                labels = [labels str2num([num2str(i) num2str(j)])];
+            end
+        end
     end
-end        
+end
 
 AAA = AA + 0.05*rand(size(AA)); % otherwise identical submissions will be condensed too closely
 DD = pdist(AAA');
 ZZ = linkage(DD);
+%dendrogram(ZZ,0, 'Labels', num2str(labels(:)))
 dendrogram(ZZ,0, 'Orientation','left', 'Labels', num2str(labels(:)))
 
 %examine close ones by hand
